c2c2f7c0deb9ee7927c54f858d42be3fbf102036
Milivoj Papec <milivoj.papec.ext@ericsson.com>
2026-08-11 16:29:41 +0200

    sliding window imporvement

--- algorithms/array/array_max_array_size_k.py
+++ algorithms/array/array_max_array_size_k.py
@@ -1,5 +1,6 @@
 array = [1,4,1,10,25,3,5,0,26]
 k = 4
+
 def array_max_array_size_k(array : list[int], k:int):
     start = 0
     end = k-1
@@ -13,5 +14,13 @@ def array_max_array_size_k(array : list[int], k:int):
         
     return max_sum
 
+def array_max_array_size_k_improved(array : list[int], k:int):
+    current_sum = sum(array[:k])
+    max_sum = sum(array[:k])
+    for i in range(len(array) - k):
+        current_sum = current_sum - array[i] + array[i +k]
+        max_sum = max(current_sum, max_sum)
+        
+    return max_sum
 
-print(array_max_array_size_k(array, k))
+print(array_max_array_size_k_improved(array, k))
