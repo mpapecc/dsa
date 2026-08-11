d9edc6afc0db17c47da124dddab6f8a96e89bcfe
Milivoj Papec <milivoj.papec.ext@ericsson.com>
2026-08-11 15:43:13 +0200

    slide window

--- algorithms/array/array_max_array_size_k.py
+++ algorithms/array/array_max_array_size_k.py
@@ -2,20 +2,15 @@ array = [1,4,1,10,25,3,5,0,26]
 k = 4
 def array_max_array_size_k(array : list[int], k:int):
     start = 0
-    end = 3
+    end = k-1
     current_sum = sum(array[:k])
     max_sum = current_sum
     while end < len(array)-1:
         start+=1
         end+=1
-        # print(start)
-        # print(end)
-        current_sum = max_sum - array[start] + array[end]
-        print(current_sum)
-        print(array[start:end+1])
+        current_sum = current_sum - array[start-1] + array[end]
         max_sum = max(current_sum, max_sum)
         
-
     return max_sum
 
 
